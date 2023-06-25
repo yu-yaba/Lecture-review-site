dd31f45c3969bb3a7d1b23948c983e96ad8c7859
yu-yaba <kawai.yuya3401@gmail.com>
2023-06-25 15:07:46 +0900

    fix/データベースをproductionでpgに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
+  root to: 'api/lectures#index'
+
   namespace :api do
     resources :lectures do
       resources :reviews, only: [:index, :create]
     end
   end
-  
-  # これを追加します。これにより、未知のルートがリクエストされたときには全てsiteコントローラーのindexアクションにルーティングします。
+
   get '*path', to: 'site#index', constraints: ->(request){ request.format.html? }
-  root to: 'site#index'end
+end
